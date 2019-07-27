80e52691f63b48d663ba2ac6ed91e6857be439d7
berkebac <49922154+berkebac@users.noreply.github.com>
2019-07-27 14:46:19 +0300

    g

---
+++ Silverlight Uyg3 Animasyon/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Silverlight_Uyg3_Animasyon
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard2.Begin();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard1.Begin();
+        }
+    }
+}
